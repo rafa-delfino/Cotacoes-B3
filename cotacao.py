154a3208b016d2840d95008a0f094563fef793ae
rafa-delfino <rafa.delfino@gmail.com>
2021-11-16 16:18:27 -0300

    Atualização

--- cotacao.py
+++ cotacao.py
@@ -7,7 +7,7 @@ from datetime import date, timedelta
 acao = ['bbas3', 'itsa4', 'ggbr4', 'mglu3', 'petr4']
 url = 'https://www.alphavantage.co/query?'
 func = 'TIME_SERIES_DAILY'
-key = #'insira sua key aqui'
+key = #'Sua key aqui'
 
 #Para cada item na lista 'Ação', vai repetir este loop.
 for a in acao:
