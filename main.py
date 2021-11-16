cdae9653473b8f690af8d3f4e2064be4ab943931
rafa-delfino <rafa.delfino@gmail.com>
2021-11-16 10:03:30 -0300

    Primeiro

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from datetime import date, timedelta
 acao = ['bbas3', 'itsa4', 'ggbr4', 'mglu3', 'petr4']
 url = 'https://www.alphavantage.co/query?'
 func = 'TIME_SERIES_DAILY'
-key = #SUA KEY AQUI
+key = '27UV5WOZ0J5UDDFN'
 
 #Para cada item na lista 'Ação', vai repetir este loop.
 for a in acao:
